eb39daa05a47f7b331c0080b20f6b180a28aee80
Igor Belyaev <IYBelyaev@alfabank.ru>
2021-09-20 18:44:34 +0300

    feat(*): adds page for user info

--- src/client/user-page/user-page.tsx
+++ src/client/user-page/user-page.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { createCn } from 'bem-react-classname';
+import { useParams } from 'react-router-dom';
+import { Typography } from '@alfalab/core-components/typography';
 
 import './user-page.css';
 
-const UserPage = () => <div>Страница юзера</div>
+const cn = createCn('user-page');
+
+const UserPage = () => {
+    const { id } = useParams<{id: string}>();
+
+    return (
+        <div className={cn()}>
+            <Typography.Title className={cn('title')} tag='h1' font='system'>
+                Страница обратной связи по скринингу
+            </Typography.Title>
+            <div className={cn('row')}>Имя:</div>
+            <div className={cn('row')}>Фамилия:</div>
+            <div className={cn('row')}>Прошел:</div>
+            <div className={cn('row')}>Дата начала скрининга:</div>
+            <div className={cn('row')}>Дата завершения скрининга:</div>
+            <div className={cn('row')}>Кол-во баллов:</div>
+            <div className={cn('row')}>Фитбек:</div>
+        </div>
+    )
+}
 
 export default UserPage;
